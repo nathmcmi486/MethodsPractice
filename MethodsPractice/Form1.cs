e7ea7f9b0b55a929a674bc86ecedb95db4ac3876
Nathan McMillan <nathmcmi486@ed.amdsb.ca>
2023-03-31 14:11:16 -0400

    Answered all questions

--- MethodsPractice/Form1.cs
+++ MethodsPractice/Form1.cs
@@ -22,7 +22,10 @@ namespace MethodsPractice
             /// TODO - Place method calls here
 
             PrintSignature();
-
+            RectangleArea();
+            GetInputSum();
+            GetCircleArea();
+            CheckLargestValue();
         }
 
         /// 1. Print a signature to the screen
@@ -39,20 +42,31 @@ namespace MethodsPractice
 
         public void RectangleArea()
         {
-
+            q2Output.Text = $"The area of 5x7 rectangle is {5 * 7} units squared";
         }
 
         /// 3. Create a method to get numbers from q3Num1Input and q3Num2Input,
         /// add them together, and then display output as follows:
         /// 
         /// 5 + 4 = 9 
-
+        public void GetInputSum()
+        {
+            int sum = Convert.ToInt32(this.q3Num1Input.Text) + Convert.ToInt32(this.q3Num2Input.Text);
+            this.q3Output.Text = $"{q3Num1Input.Text} + {q3Num2Input.Text} = {sum}";
+        }
 
         
         /// 4. Create a method to calculate the area of a circle based on  
         /// the radius entered in radiusInput, then display output as follows:
         /// 
         /// The area of a circle with radius of 4 is 50.24 units squared
+        public void GetCircleArea()
+        {
+            double pi = 3.14159;
+            double r = 4.0;
+
+            this.q4Output.Text = $"The area of a circle with a radius of 4 is ~{pi * (r * r)} units squared";
+        }
 
 
 
@@ -62,6 +76,23 @@ namespace MethodsPractice
         /// 
         /// Inputs: 5, 7, 2
         /// he largest value is 7
+        public void CheckLargestValue()
+        {
+            int num1 = Convert.ToInt32(this.q5Num1Input.Text);
+            int num2 = Convert.ToInt32(this.q5Num2Input.Text);
+            int num3 = Convert.ToInt32(this.q5Num3Input.Text);
+            int largest_value = 0;
+
+            if (num1 >= num2 && num1 >= num3)
+            {
+                largest_value = num1;
+            } else if (num2 >= num1 && num2 >= num3)
+            {
+                largest_value = num2;
+            } else { largest_value = num3; }
+
+            this.q5Output.Text = $"The largest value is {largest_value}";
+        }
 
 
     }
